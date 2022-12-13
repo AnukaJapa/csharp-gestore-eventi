2614da73be2f249891520bac8697475c97d7aa6c
AnukaJapa <japaridze.ana96@gmail.com>
2022-12-13 12:16:07 +0100

    stampa a video posti prenotati e disponibili

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -13,4 +13,12 @@ int capienzaMassimaDellEvento= int.Parse(Console.ReadLine());
 
 Evento event1 = new Evento(titoloDellEvento, dataDellEvento,capienzaMassimaDellEvento);
 
+Console.WriteLine("quante prenotazioni vuoi effettuare?");
+int prenotazioniDaEffettuare = int.Parse(Console.ReadLine());
+event1.PrenotaPosti(prenotazioniDaEffettuare);
+
+Console.WriteLine($@"
+il numero di posti prenotati: {event1.GetPostiPrenotati()}
+il numero di posti disponibili: {event1.GetCapienzaMassima() -event1.GetPostiPrenotati()}
+");
 
